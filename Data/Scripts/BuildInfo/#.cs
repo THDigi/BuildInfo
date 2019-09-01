90f26026166b93faf3ad9a8c0a35c148d71538ba
Digi <hunter_digital_sl@yahoo.com>
2019-09-01 03:37:58 +0300

    Added comments to #.cs to explain its existence.

---
+++ Data/Scripts/BuildInfo/#.cs
@@ -0,0 +1,2 @@
+// This file is needed to avoid an issue with the mod script loader.
+// If it loads a file from any subfolder it will use that folder as the root and ignore folders under it, causing compile errors.
