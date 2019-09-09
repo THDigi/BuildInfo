01b152760a8ade3500422538350d18ec857face1
Digi <hunter_digital_sl@yahoo.com>
2019-09-09 15:49:05 +0300

    Added testing purposes quick-copy batch script.

---
+++ Data/Scripts/BuildInfo/copy to DS and client.bat
@@ -0,0 +1,5 @@
+rmdir C:\Users\Digi\AppData\Roaming\SpaceEngineersDedicated\content\244850\513466522\Data\Scripts\BuildInfo\ /S /Q
+
+robocopy.exe .\ C:\Steam\steamapps\workshop\content\244850\513466522\Data\Scripts\BuildInfo\ *.cs /xd "bin" "obj" ".vs" "DoNotCopy_VanillaDataCompare" /S
+
+pause
