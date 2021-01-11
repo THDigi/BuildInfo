ff316e6e5586bb4c599a8e0b94f08f4ad26d97c9
Digi <hunter_digital_sl@yahoo.com>
2021-01-11 14:55:08 +0200

    Incremented mod version for players to be notified of new features

--- Data/Scripts/BuildInfo/Constants.cs
+++ Data/Scripts/BuildInfo/Constants.cs
@@ -11,7 +11,7 @@ namespace Digi.BuildInfo
 {
     public class Constants : ModComponent
     {
-        public const int MOD_VERSION = 2; // notifies player of notable changes and links them to workshop's changelog.
+        public const int MOD_VERSION = 3; // notifies player of notable changes and links them to workshop's changelog.
 
         public readonly Vector2 BLOCKINFO_SIZE = new Vector2(0.02164f, 0.00076f);
         public const float ASPECT_RATIO_54_FIX = 0.938f;
