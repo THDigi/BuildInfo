bbe723cde84f2d37bdeb68bc9a5e3faaf491f513
Digi <hunter_digital_sl@yahoo.com>
2021-02-10 19:46:18 +0200

    Updated project stuff

--- Data/Scripts/BuildInfo/copy to DS and client.bat
+++
@@ -1,7 +0,0 @@
-rmdir C:\Users\Digi\AppData\Roaming\SpaceEngineersDedicated\content\244850\513466522\Data\Scripts\BuildInfo\ /S /Q
-rmdir C:\Steam\steamapps\workshop\content\244850\513466522\Data\Scripts\BuildInfo\ /S /Q
-
-robocopy.exe .\ C:\Steam\steamapps\workshop\content\244850\513466522\Data\Scripts\BuildInfo\ *.cs /xd "bin" "obj" ".vs" "DoNotCopy_VanillaDataCompare" /S
-robocopy.exe .\ C:\Users\Digi\AppData\Roaming\SpaceEngineersDedicated\content\244850\513466522\Data\Scripts\BuildInfo\ *.cs /xd "bin" "obj" ".vs" "DoNotCopy_VanillaDataCompare" /S
-
-pause
