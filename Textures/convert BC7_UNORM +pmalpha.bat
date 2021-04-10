a3222cfc05ff97eb452be6791298fcddf156262d
Digi <hunter_digital_sl@yahoo.com>
2021-04-10 23:23:56 +0300

    Changed TransparentGradient.dds to be sRGB

--- Textures/convert BC7_UNORM +pmalpha.bat
+++ Textures/convert BC7_UNORM +pmalpha.bat
@@ -1,5 +1,6 @@
 echo off
 cls
 C:\Programs\Texconv\texconv.exe *.png -nologo -y -f BC7_UNORM -pmalpha -if LINEAR_DITHER
+C:\Programs\Texconv\texconv.exe TransparentGradient.png -nologo -y -f BC7_UNORM_sRGB -pmalpha -if LINEAR_DITHER
 ren *.DDS *.dds
 pause
