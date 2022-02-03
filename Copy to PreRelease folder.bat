8cde0e9c2243916a440e9d86bcfddafee719c5a7
Digi <hunter_digital_sl@yahoo.com>
2022-02-03 10:15:06 +0200

    Internal: no copy of *.ps1

--- Copy to PreRelease folder.bat
+++ Copy to PreRelease folder.bat
@@ -3,6 +3,6 @@ set REPLACE_IN_PATH=%APPDATA%\SpaceEngineers\Mods\BuildInfoPreRelease
 
 rmdir "%REPLACE_IN_PATH%" /S /Q
 
-robocopy.exe .\ "%REPLACE_IN_PATH%" *.* /S /xd .git bin obj .vs ignored DoNotCopy_VanillaDataCompare /xf *.exe *.dll *.lnk *.git* *.bat *.zip *.7z *.blend* *.png *.md *.log *.sln *.csproj *.csproj.user *.ruleset desktop.ini
+robocopy.exe .\ "%REPLACE_IN_PATH%" *.* /S /xd .git bin obj .vs ignored DoNotCopy_VanillaDataCompare /xf *.exe *.dll *.lnk *.git* *.bat *.zip *.7z *.blend* *.png *.md *.log *.sln *.csproj *.csproj.user *.ruleset *.ps1 desktop.ini
 
 pause
