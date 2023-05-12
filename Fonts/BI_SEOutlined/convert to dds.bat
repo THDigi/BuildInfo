c78f375da08a65ab8b548f2196e735869f03fd82
Digi <844339+THDigi@users.noreply.github.com>
2023-05-12 11:23:11 +0300

    Fonts: Added platform icons
    Also font is ever so slightly more shadowy

--- Fonts/BI_SEOutlined/convert to dds.bat
+++ Fonts/BI_SEOutlined/convert to dds.bat
@@ -1,5 +1,5 @@
 echo off
 cls
-C:\Programs\Texconv\texconv.exe *.png -nologo -y -f BC7_UNORM_SRGB -pmalpha -if TRIANGLE_DITHER_DIFFUSION -m 2
+C:\Programs\Texconv\texconv.exe FontData*.png -nologo -y -f BC7_UNORM_SRGB -pmalpha -if TRIANGLE_DITHER_DIFFUSION -m 2
 ren *.DDS *.dds
 pause
