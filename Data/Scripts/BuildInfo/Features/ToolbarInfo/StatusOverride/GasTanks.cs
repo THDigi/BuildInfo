2070ec57361bf34ba3743834cc6ba2c22cfba2d5
Digi <844339+THDigi@users.noreply.github.com>
2023-07-07 14:49:40 +0300

    ToolbarStatus: Added Stockpile custom status to indicate fill %
    Single and group too.

--- Data/Scripts/BuildInfo/Features/ToolbarInfo/StatusOverride/GasTanks.cs
+++ Data/Scripts/BuildInfo/Features/ToolbarInfo/StatusOverride/GasTanks.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Digi.BuildInfo.Utilities;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Common.ObjectBuilders.Definitions;
@@ -13,13 +14,24 @@ namespace Digi.BuildInfo.Features.ToolbarInfo.StatusOverride
     {
         public GasTanks(ToolbarStatusProcessor processor) : base(processor)
         {
-            StatusDel func = Refill;
-            processor.AddStatus(typeof(MyObjectBuilder_GasTank), func, "Refill");
-            processor.AddStatus(typeof(MyObjectBuilder_OxygenTank), func, "Refill");
+            Register(Refill, GroupRefill, "Refill");
 
-            GroupStatusDel groupFunc = GroupRefill;
-            processor.AddGroupStatus(typeof(MyObjectBuilder_GasTank), groupFunc, "Refill");
-            processor.AddGroupStatus(typeof(MyObjectBuilder_OxygenTank), groupFunc, "Refill");
+            Register(Stockpile, GroupStockpile, "Stockpile", "Stockpile_On", "Stockpile_Off");
+        }
+
+        void Register(StatusDel singleDel, GroupStatusDel groupDel, string actionId1, string actionId2 = null, string actionId3 = null, string actionId4 = null, string actionId5 = null, string actionId6 = null)
+        {
+            if(singleDel != null)
+            {
+                Processor.AddStatus(typeof(MyObjectBuilder_GasTank), singleDel, actionId1, actionId2, actionId3, actionId4, actionId5, actionId6);
+                Processor.AddStatus(typeof(MyObjectBuilder_OxygenTank), singleDel, actionId1, actionId2, actionId3, actionId4, actionId5, actionId6);
+            }
+
+            if(groupDel != null)
+            {
+                Processor.AddGroupStatus(typeof(MyObjectBuilder_GasTank), groupDel, actionId1, actionId2, actionId3, actionId4, actionId5, actionId6);
+                Processor.AddGroupStatus(typeof(MyObjectBuilder_OxygenTank), groupDel, actionId1, actionId2, actionId3, actionId4, actionId5, actionId6);
+            }
         }
 
         bool Refill(StringBuilder sb, ToolbarItem item)
@@ -129,6 +141,68 @@ namespace Digi.BuildInfo.Features.ToolbarInfo.StatusOverride
                 sb.NumberCappedSpaced((int)noGas, MaxChars - 5).Append("NoGas\n");
                 sb.NumberCappedSpaced((int)emptyBottles, MaxChars - 5).Append("Bottl"); // typo on purpose to have 3 spaces for "99+" numbers
             }
+
+            return true;
+        }
+
+        // TODO: better stockpile on/off text?
+        // TODO: consistency between fillable blocks (batteries, tanks, jumpdrives) in text for single and group! (esp percentage position)
+
+        bool Stockpile(StringBuilder sb, ToolbarItem item)
+        {
+            IMyGasTank tank = (IMyGasTank)item.Block;
+
+            int filledPercent = (int)Math.Round(tank.FilledRatio * 100);
+            sb.NumberCapped(filledPercent, 4).Append('%');
+
+            sb.Append('\n');
+
+            sb.Append(tank.Stockpile ? "Fill" : "Auto");
+
+            return true;
+        }
+
+        bool GroupStockpile(StringBuilder sb, ToolbarItem groupToolbarItem, GroupData groupData)
+        {
+            if(!groupData.GetGroupBlocks<IMyGasTank>())
+                return false;
+
+            int off = 0;
+            int broken = 0;
+            int stockpile = 0;
+            double filled = 0;
+            double capacity = 0;
+
+            foreach(IMyGasTank tank in groupData.Blocks)
+            {
+                if(!tank.IsFunctional)
+                    broken++;
+
+                if(!tank.Enabled)
+                    off++;
+
+                if(tank.Stockpile)
+                    stockpile++;
+
+                filled += tank.FilledRatio * tank.Capacity;
+                capacity += tank.Capacity;
+            }
+
+            int total = groupData.Blocks.Count;
+
+            Processor.AppendGroupStats(sb, broken, off);
+
+            int filledPercent = (int)Math.Round((filled / capacity) * 100);
+            sb.NumberCapped(filledPercent, 4).Append('%');
+
+            sb.Append('\n');
+
+            if(stockpile == total)
+                sb.Append("Fill");
+            else if(stockpile == 0)
+                sb.Append("Auto");
+            else
+                sb.Append("(Mixed)");
 
             return true;
         }
