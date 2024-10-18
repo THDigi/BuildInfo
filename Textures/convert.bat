4d152b5e205f08b50f6885ca42498ced8826dcae
Digi <844339+THDigi@users.noreply.github.com>
2024-10-18 05:12:21 +0300

    Internal: Added LaserGradientSRGB transparent material

--- Textures/convert.bat
+++ Textures/convert.bat
@@ -4,6 +4,7 @@ cls
 C:\Programs\Texconv\texconv.exe *.png -nologo -y -f BC7_UNORM -pmalpha -if LINEAR_DITHER
 
 C:\Programs\Texconv\texconv.exe TransparentGradientSRGB.png -nologo -y -f BC7_UNORM_sRGB -pmalpha -if LINEAR_DITHER
+C:\Programs\Texconv\texconv.exe LaserGradientSRGB.png -nologo -y -f BC7_UNORM_sRGB -pmalpha -if LINEAR_DITHER
 
 ren *.DDS *.dds
 
